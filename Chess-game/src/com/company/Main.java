1e343874b057f753792611addd6795070df4789e
Vitor Galbier <vitor.galbier@gmail.com>
2022-05-25 15:17:49 -0300

    uploading board and pieces

--- Chess-game/src/com/company/Main.java
+++ Chess-game/src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import Boardgame.Board;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Board board = new Board(8, 8);
     }
 }
